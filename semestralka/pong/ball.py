df075170c4c783e48feee7661a1843edb2087ebf
Matej <kulhama8@fit.cvut.cz>
2022-12-26 21:18:50 +0100

    upravy lopty

--- semestralka/pong/ball.py
+++ semestralka/pong/ball.py
@@ -1,22 +1,32 @@
-import screen
+"""counterclockwise angle between a point and the x axis"""
+from math import atan2
 import pygame
-import numpy as np
 
 class Ball:
-    ball_x = screen.WIDTH/2
-    ball_y = screen.HEIGHT/2
+    """ball object"""
 
-    def __init__(self, radius, x_velocity, y_velocity):
+    def __init__(self, radius, x_velocity, y_velocity, win):
+        """creates a ball object"""
+        self.pos_x = win.get_width()/2 #ball will be in the center at the beginning
+        self.pos_y = win.get_height()/2
+
         self.radius = radius
+
         self.x_velocity = x_velocity
         self.y_velocity = y_velocity
 
-    def move(self):
-        self.ball_x += self.x_velocity
-        self.ball_y += self.y_velocity
 
-    def draw(self):
-        pygame.draw.circle(screen.screen, screen.WHITE, (self.ball_x, self.ball_y), self.radius)
-    
+    def move(self):
+        """moves the ball depending on its velocity"""
+        self.pos_x += self.x_velocity
+        self.pos_y += self.y_velocity
+
+
+    def draw(self, win):
+        """draws the ball on the screen"""
+        pygame.draw.circle(win, (255, 255, 255), self.pos_x, self.pos_y, self.radius)
+
+
     def angle(self):
-        return np.arccos(np.abs(self.x_velocity)/(np.sqrt(self.x_velocity**2 + self.y_velocity**2)))
+        """returns angle of the ball and the x axis in radians"""
+        return atan2(self.y_velocity, self.x_velocity)
