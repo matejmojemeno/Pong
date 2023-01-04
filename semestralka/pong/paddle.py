7a5d822104e007f1abf071eaa43bc14e7d636fcf
Matej <kulhama8@fit.cvut.cz>
2023-01-05 00:26:41 +0100

    trochu upravene paddle

--- semestralka/pong/paddle.py
+++ semestralka/pong/paddle.py
@@ -1,42 +1,60 @@
-"""numpy clip for clipping paddle position"""
+"""numpy clip for restraining the paddle from moving out of screen"""
 from numpy import clip
 import pygame
 
 class Paddle:
-    """paddle class"""
+    """a paddle object"""
+    BASE_HEIGHT = 80
+    BASE_WIDTH = 20
+    VELOCITY = 10
 
-    def __init__(self, x, y, win):
+    def __init__(self, left, size, speed, win):
         """creates a paddle object"""
-        self.pos_x = x
-        self.pos_y = y
-        self.width = self.get_width(win)
-        self.height = self.get_height(win)
+        
+        self.width = self.BASE_WIDTH + size
+        self.height = self.BASE_HEIGHT + size*5
+
+        self.set_x(left, win)
+        self.set_y(win)
+
+        #TODO dat tomu nejaku normalnu rychlost
+        self.velocity = self.VELOCITY + speed
+        self.sprinting_velocity = 0
 
 
-    def get_width(self, win):
-        """calculates width of the paddle based on window size"""
-        return win.get_width()/100
+    def set_x(self, left, win):
+        if left:
+            self.pos_x = 20
+        else:
+            self.pos_x = win.get_width() - 20 - self.width
 
 
-    def get_height(self, win):
-        """calculates height of the paddle based on window size"""
-        return win.get_height()/8
+    def set_y(self, win):
+        self.pos_y = win.get_height()/2 - self.height/2
 
 
     def draw(self, win):
         """draws the paddle"""
-        pygame.draw.rect(win, (255, 255, 255), self.pos_x, self.pos_y, self.width, self.height)
+        pygame.draw.rect(win, (255, 255, 255), (self.pos_x, self.pos_y, self.width, self.height))
 
 
     def adjust_paddle(self, win):
         """stops the paddle from going out of the screen"""
-        clip(0, win.get_height() - self.height, self.pos_y)
+        self.pos_y = clip(a_min=0, a_max=win.get_height() - self.height, a=self.pos_y)
 
 
-    def move(self, velocity, up, win):
+    def move(self, up, win):
         """moves the paddle up and down"""
         if up:
-            self.pos_y -= velocity
+            self.pos_y -= max(self.velocity, self.sprinting_velocity)
         else:
-            self.pos_y += velocity
+            self.pos_y += max(self.velocity, self.sprinting_velocity)
         self.adjust_paddle(win)
+
+
+    def sprint(self, is_sprinting):
+        """make the paddle 50% faster"""
+        if is_sprinting:
+            self.sprinting_velocity = self.velocity*1.5
+        else:
+            self.sprinting_velocity = 0
