ccae7eed2e10c878ccbc68f87b0c1599fc63222a
Matej <kulhama8@fit.cvut.cz>
2023-01-06 19:08:12 +0100

    hadam to uz je dobre

--- semestralka/pong/collision.py
+++ semestralka/pong/collision.py
@@ -47,8 +47,13 @@ def new_direction(ball, paddle):
     return (velocity * np.cos(angle), - velocity * np.sin(angle))
 
 
-def corner_bounce(ball):
-    ball.x_velocity *= -1
+def corner_bounce(ball, paddle):
+    direction = - np.sign(ball.x_velocity)
+    angle = np.pi/4 * np.sign(paddle.pos_y + paddle.height/2 - ball.pos_y)
+    velocity = np.clip(0, ball.RADIUS, ball.total_velocity() * BOUNCE_INCREASE)
+
+    ball.x_velocity = velocity * np.cos(angle) * direction
+    ball.y_velocity = - velocity * np.sin(angle)
 
 
 def front_bounce(ball, paddle):
@@ -57,19 +62,17 @@ def front_bounce(ball, paddle):
 
     ball.x_velocity *= -direction
 
+
 def top_bounce(ball):
     ball.y_velocity *= -1
 
 
 def bounce(ball, paddle, front_intersect, top_intersect):
-    if front_intersect and top_intersect:
-        # print(1)
-        corner_bounce(ball)
+    if front_intersect and top_intersect and not (paddle.pos_y < ball.pos_y < paddle.pos_y + paddle.height):
+        corner_bounce(ball, paddle)
     elif front_intersect:
-        # print(2)
         front_bounce(ball, paddle)
     elif top_intersect:
-        # print(3)
         top_bounce(ball)
 
 
@@ -81,14 +84,16 @@ def ball_paddle_collision(ball, paddle):
     else:
         front_intersect = False
 
-    if ball.y_velocity < 0:
-        top_intersect = top_paddle_intersect(ball, paddle.pos_x, paddle.pos_y + paddle.height, paddle.width)
-    else:
-        top_intersect = top_paddle_intersect(ball, paddle.pos_x, paddle.pos_y, paddle.width)
+    top_intersect = top_paddle_intersect(ball, paddle.pos_x, paddle.pos_y + paddle.height, paddle.width)\
+                    or top_paddle_intersect(ball, paddle.pos_x, paddle.pos_y, paddle.width)
     
     bounce(ball, paddle, front_intersect, top_intersect)
+
+    return front_intersect or top_intersect
     
 
 def top_bottom_collision(ball, win):
-    if ball.pos_y + ball.RADIUS >= win.get_height() or ball.pos_y - ball.RADIUS <= 0:
+    if ball.pos_y + ball.RADIUS >= win.get_height() and ball.y_velocity > 0:
+        ball.y_velocity *= -1
+    elif ball.pos_y - ball.RADIUS <= 0 and ball.y_velocity < 0:
         ball.y_velocity *= -1
