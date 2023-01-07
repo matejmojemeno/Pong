b581bacb74513bf3966f58cd2faf255885d49cd5
Matej <kulhama8@fit.cvut.cz>
2023-01-08 00:39:12 +0100

    read skins

--- semestralka/pong/readplayer.py
+++ semestralka/pong/readplayer.py
@@ -1,21 +1,22 @@
 PATH = 'resources/'
-FILE = 'player_stats.txt'
+STATS_FILE = 'player_stats.txt'
+SKINS_FILE = 'skins/skins_info.txt'
 
 
 def read_stats():
-    with open(PATH + FILE, 'r') as file:
+    with open(PATH + STATS_FILE, 'r') as file:
         stats = [int(line) for line in file.readlines()]
     return stats[:-1]
 
 
 def read_coins():
-    with open(PATH + FILE, 'r') as file:
+    with open(PATH + STATS_FILE, 'r') as file:
         lines = file.readlines()
     return int(lines[-1])
 
 
 def save(stats, coins):
-    with open(PATH + FILE, 'w') as file:
+    with open(PATH + STATS_FILE, 'w') as file:
         for stat in stats:
             file.write(str(stat) + '\n')
         file.write(str(coins) + '\n')
@@ -26,3 +27,21 @@ def add_money(amount):
     coins = read_coins() + amount
 
     save(stats, coins)
+
+
+def read_skins():
+    with open(PATH + SKINS_FILE, 'r') as file:
+        curr_skin = int(file.readline())
+
+        skins = []
+
+        for line in file:
+            # Split the line into two parts: the string and the bool
+            
+            string, boolean = line.strip().split()
+            
+            # Convert the boolean string to a bool
+            boolean = boolean == 'True'
+            skins.append((string, boolean))
+    
+    return (curr_skin, skins)
