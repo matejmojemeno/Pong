a8aec901f10292c62f05a9a7dc6aa7793b144b51
Matej <kulhama8@fit.cvut.cz>
2023-01-07 15:16:03 +0100

    save

--- semestralka/pong/readplayer.py
+++ semestralka/pong/readplayer.py
@@ -5,10 +5,17 @@ FILE = 'player_stats.txt'
 def read_stats():
     with open(PATH + FILE, 'r') as file:
         stats = [int(line) for line in file.readlines()]
-    return tuple(stats[:-1])
+    return stats[:-1]
 
 
 def read_coins():
     with open(PATH + FILE, 'r') as file:
         lines = file.readlines()
     return int(lines[-1])
+
+
+def save(stats, coins):
+    with open(PATH + FILE, 'w') as file:
+        for stat in stats:
+            file.write(str(stat) + '\n')
+        file.write(str(coins) + '\n')
