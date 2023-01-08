dfe3155ea1e59da601834e6a0763d2db1c650e63
Matej <kulhama8@fit.cvut.cz>
2023-01-08 18:22:52 +0100

    este pridana lopticka

--- semestralka/pong/opponent.py
+++ semestralka/pong/opponent.py
@@ -6,49 +6,50 @@ from .paddle import Paddle
 class Opponent:
     """opponent class"""
 
-    def __init__(self, win, difficulty):
+    def __init__(self, ball, win, difficulty):
         """create opponent object"""
 
         # every difficulty adds 5 to stats so hard is 15, 15, 15
         self.paddle = Paddle(False, difficulty * 5, difficulty * 5, win)
         self.difficulty = difficulty
         self.score = 0
+        self.ball = ball
 
-    def move(self, win, ball):
+    def move(self, win):
         """move the paddle"""
 
         # decide the paddle movement based on x velocity of ball
         if abs(
             self.paddle.pos_x -
-            ball.pos_x -
-            ball.x_velocity) > abs(
+            self.ball.pos_x -
+            self.ball.x_velocity) > abs(
             self.paddle.pos_x -
-                ball.pos_x):
+                self.ball.pos_x):
             self.center(win)
         else:
             if self.difficulty < 2:
                 # for easy and medium
-                self.follow_by_edges(ball, win)
+                self.follow_by_edges(self.ball, win)
             else:
                 # got better results in testing so we use this for hard
-                self.follow_by_center(ball, win)
+                self.follow_by_center(self.ball, win)
 
-    def follow_by_edges(self, ball, win):
+    def follow_by_edges(self, win):
         """move to the ball when it goes outside paddle y"""
 
-        if self.paddle.pos_y > ball.pos_y:
+        if self.paddle.pos_y > self.ball.pos_y:
             self.paddle.move(True, win)
-        if self.paddle.pos_y + self.paddle.height < ball.pos_y:
+        if self.paddle.pos_y + self.paddle.height < self.ball.pos_y:
             self.paddle.move(False, win)
 
-    def follow_by_center(self, ball, win):
+    def follow_by_center(self, win):
         """follow the ball to always match center of paddle and center of ball"""
 
         if self.paddle.pos_y + self.paddle.height / \
-                2 - self.paddle.velocity > ball.pos_y:
+                2 - self.paddle.velocity > self.ball.pos_y:
             self.paddle.move(True, win)
         if self.paddle.pos_y + self.paddle.height / \
-                2 + self.paddle.velocity < ball.pos_y:
+                2 + self.paddle.velocity < self.ball.pos_y:
             self.paddle.move(False, win)
 
     def center(self, win):
